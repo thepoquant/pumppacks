9663af13558a989f2e42447819539cf6836bceb7
thepo <thepo@users.noreply.github.com>
2026-06-01 15:46:48 -0700

    fix: remove remaining blink sdk imports from main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BlinkUIProvider, Toaster } from '@blinkdotnew/ui'
+
 import App from './App'
 import './index.css'
 
@@ -10,12 +10,9 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <BlinkUIProvider theme="linear" darkMode="dark">
-          <Toaster />
-          <div className="flex w-full flex-1 flex-col min-h-0">
-            <App />
-          </div>
-        </BlinkUIProvider>
+        <div className="flex w-full flex-1 flex-col min-h-0">
+          <App />
+        </div>
       </QueryClientProvider>
   </React.StrictMode>,
 )
